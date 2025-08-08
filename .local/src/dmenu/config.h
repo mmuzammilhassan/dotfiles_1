992dc3b97b3ff91ee0da7831116dd565e3cdd5af
ze6hyr <aspas072@gmail.com>
2025-08-08 12:31:52 +0500

    push sub directorys

---
+++ .local/src/dmenu/config.h
@@ -0,0 +1,38 @@
+//search when do /* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+//    "FiraCodeNerdFont:pixelsize=13:antialias=true:autohint=true",
+ //   "FiraCodeNerdFont:pixelsize=13:antialias=true:autohint=true"
+     "FiraCode:bold:size=16",
+    //"JetBrains Mono:size=16",
+    "JoyPixels:pixelsize=16:antialias=true:autohint=true"
+//	"PxPlus IBM VGA 8x16:size=16",
+//	"PxPlus IBM VGA 8x16:pixelsize=16:antialias=true:autohint=true"
+};
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 9;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
